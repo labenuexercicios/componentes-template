ba68fea0e2a15d8e43ecae96488df42885e78f93
euleo11 <leonardov.silva15@gmail.com>
2022-10-11 13:46:15 -0300

    introdução ao react

---
+++ leonardo-victor-a031/src/componentes/Carros.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+export default function Carros(){
+    return(
+        <div>
+            <h2>HB20S</h2>
+            <ul>
+                <li>Cor: Prata</li>
+                <li>Ano: 2013/2014</li>
+                <li>Flex: true</li>
+            </ul>
+        </div>
+    )
+}
---
+++ leonardo-victor-a031/src/componentes/Garagem.js
@@ -0,0 +1,12 @@
+import React from "react"
+
+
+export default function Garagem () {
+    const nome= "Leonardo"
+    return(
+        
+        <>
+        <h1>Garagem de {nome}</h1>
+        </>
+    )
+}
