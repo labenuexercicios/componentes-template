2e76cf01288022625daba6c66f1aa7557a74e210
raissanfg <raissanfg@gmail.com>
2023-10-24 13:36:38 -0300

    exercicios-template-components

---
+++ template/src/components/Carro.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+export default function Carro (){
+    return(
+        <>
+        <h2> Carro</h2>
+            <ul>
+                <li> Modelo: Fusca</li>
+                <li> Flex: não</li>
+                <li> Cor: Azul</li>
+            </ul>
+        </>
+    )
+}
---
+++ template/src/components/Garagem.js
@@ -0,0 +1,16 @@
+import React from "react";
+import  Carro from "./Carro";
+
+export default function Garagem() {
+    return (
+        <>
+            <h1> Component Garagem</h1>
+            <Carro />
+            <Carro />  
+            <Carro />
+
+
+        </>
+
+    );
+}
