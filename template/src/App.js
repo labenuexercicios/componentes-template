8bc4141e0a1588a4473c420e81f9aa1ae1a296ae
raissanfg <raissanfg@gmail.com>
2023-10-26 16:21:18 -0300

    STYLED COMPONENTS

--- template/src/App.js
+++ template/src/App.js
@@ -1,9 +1,16 @@
 import React from "react";
-import "./App.css";
-import Header from "./components/Header";
-import Garagem from "./components/Garagem";
-import Footer from "./components/Footer";
-import "./styles.css";
+//import Header from "./components/Header";
+import Garagem from "./Garagem/Garagem";
+//import Footer from "./components/Footer";
+//import "./styles.css";
+import { GlobalStyle } from "./components/GlobalStyle";
+import { Estacionamento } from "./Garagem/style";
+import FuscaImage from "./img/fusca.jpg";
+import KombiImage from "./img/kombi.jpg";
+import OpalaImage from "./img/Opala.jpg";
+import CorselImage from "./img/Corsel.jpg";
+import BrasiliaImage from "./img/brasilia.jpg";
+
 
 
 export default function App() {
@@ -14,60 +21,74 @@ export default function App() {
     alert(`Boas vindas a garagem de ${nome}`);
   }
 
+
+
   const carro = {
     modelo: "Fusca",
     cor: "azul",
     ano: 1974,
-    flex: "não"
+    flex: "não",
+    imagem: FuscaImage
   };
 
   const carro2 = {
     modelo: "Kombi",
     cor: "verde",
     ano: 1994,
-    flex: "não"
+    flex: "não",
+    imagem: KombiImage
   };
 
   const carro3 = {
-    modelo: "Variante",
+    modelo: "Opala",
     cor: "amarelo",
     ano: 1972,
-    flex: "não"
+    flex: "não",
+    imagem: OpalaImage
   };
 
   const carro4 = {
-    modelo: "ka",
+    modelo: "Corsel",
     cor: "cinza",
     ano: 1964,
-    flex: "sim"
+    flex: "sim",
+    imagem: CorselImage
   };
 
   const carro5 = {
-    modelo: "Corola",
+    modelo: "Brasilia",
     cor: "azul",
     ano: 2000,
-    flex: "sim"
+    flex: "sim",
+    imagem: BrasiliaImage
   };
 
   return (
-    <div className="App">
-      <Garagem
-        nomeGaragem={nome}
-        apresentaGaragem={apresentaGaragem}
-        carro1={carro}
-        carro2={carro2}
-        carro3={carro3}
-        carro4={carro4}
-      />
 
-      <Garagem
-        nomeGaragem={nome2}
-        apresentaGaragem={apresentaGaragem}
-        carro1={carro2}
-        carro2={carro5}
-        carro3={carro3}
-        carro4={carro4}
-      />
+    <div className="App">
+      <GlobalStyle />
+      <Estacionamento>
+        <Garagem
+          nomeGaragem={nome}
+          apresentaGaragem={apresentaGaragem}
+          carro1={carro}
+          carro2={carro2}
+          carro3={carro3}
+          carro4={carro4}
+        />
+
+        <Garagem
+          nomeGaragem={nome2}
+          apresentaGaragem={apresentaGaragem}
+
+          carro1={carro3}
+          carro2={carro4}
+          carro3={carro}
+          carro4={carro5}
+        />
+      </Estacionamento>
+
     </div>
+
   );
 }
--- template/src/components/Carro/Carro.js
+++ template/src/components/Carro/Carro.js
@@ -5,9 +5,11 @@ export default function Carro ( {carro}){
         <>
         <h2> {carro.modelo}</h2>
             <ul>
+         
                 <li> Cor: {carro.cor}</li>
                 <li> Flex: {carro.flex}</li>
                 <li> Ano: {carro.ano}</li>
+                
             </ul>
         </>
     )
